1460ce8ad51ff35abf52ba622f6b8b529ca1bb2b
Mihaela Stavila <ms@inleadmedia.com>
2017-04-03 13:11:11 +0300

    EASYOPAC-426 - Add fixe in js file.

--- js/ding_header_opening_hours.js
+++ js/ding_header_opening_hours.js
@@ -10,15 +10,15 @@
         $('#block-ding-header-opening-hours-ding-header-opened-today-all', context).hide();
 
         $('.pane-opened-libraries').on('mouseenter', function() {
-          $('#block-ding-header-opening-hours-ding-header-opened-today-all', context).toggle();
+          $('#block-ding-header-opening-hours-ding-header-opened-today-all', context).show();
         });
 
-        $('.panel-pane pane-opened-libraries').on('mouseleave', function() {
-          $('#block-ding-header-opening-hours-ding-header-opened-today-all', context).toggle();
+        $('.pane-opened-libraries').on('mouseleave', function() {
+          $('#block-ding-header-opening-hours-ding-header-opened-today-all', context).hide();
         });
 
-        $('.oh-plus').on('click', function() {
-          if($('body').hasClass('responsive-layout-mobile')) return;
+        $('.oh-plus').click(function() {
+          if(!$('body').hasClass('responsive-layout-mobile')) return;
           $('#block-ding-header-opening-hours-ding-header-opened-today-all', context).toggle();
         });
       });
