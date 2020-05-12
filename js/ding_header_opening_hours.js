99b5e7609c8397f49c19ec00f159623a7ee0f5dc
Nicolae Grigoriu <ng@inlead.dk>
2020-05-12 10:52:50 +0300

    NO-TASK - Replace only rendered panes.

--- js/ding_header_opening_hours.js
+++ js/ding_header_opening_hours.js
@@ -1,30 +1,33 @@
-(function($){
+(function ($) {
   "use strict";
 
   Drupal.behaviors.ding_header_opening_hours = {
-    attach: function(context, settings) {
+    attach: function (context, settings) {
+      var $libraries_pane = $('.pane-opened-libraries', context);
       // Move pane in the top bar menu.
-      $('li.topbar-link-opening-hours', context).find('a').replaceWith($('.pane-opened-libraries'));
+      if ($libraries_pane.length > 0) {
+        $('li.topbar-link-opening-hours', context).find('a').replaceWith($libraries_pane);
 
-      $('.pane-opened-libraries', context).once('opened', function() {
-        // Adding "plus" sign.
-        $('<span class="icon-clock"></span>').insertBefore($('#block-ding-header-opening-hours-ding-header-opened-today'));
+        $('.pane-opened-libraries', context).once('opened', function () {
+          // Adding "plus" sign.
+          $('<span class="icon-clock"></span>').insertBefore($('#block-ding-header-opening-hours-ding-header-opened-today'));
 
-        $('#block-ding-header-opening-hours-ding-header-opened-today-all', context).hide();
-
-        $('.pane-opened-libraries').on('mouseenter', function() {
-          $('#block-ding-header-opening-hours-ding-header-opened-today-all', context).show();
-        });
-
-        $('.pane-opened-libraries').on('mouseleave', function() {
           $('#block-ding-header-opening-hours-ding-header-opened-today-all', context).hide();
-        });
 
-        $('.oh-plus').on('click', function() {
-          if(!$('body').hasClass('responsive-layout-mobile')) return;
-          $('#block-ding-header-opening-hours-ding-header-opened-today-all', context).toggle();
+          $libraries_pane.on('mouseenter', function () {
+            $('#block-ding-header-opening-hours-ding-header-opened-today-all', context).show();
+          });
+
+          $libraries_pane.on('mouseleave', function () {
+            $('#block-ding-header-opening-hours-ding-header-opened-today-all', context).hide();
+          });
+
+          $('.oh-plus').on('click', function () {
+            if (!$('body').hasClass('responsive-layout-mobile')) return;
+            $('#block-ding-header-opening-hours-ding-header-opened-today-all', context).toggle();
+          });
         });
-      });
+      }
     }
   };
 })(jQuery);
